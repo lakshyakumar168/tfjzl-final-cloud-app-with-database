da23570a4d898799c486c449e970f2030d98d342
Lakshya Kumar <114494222+lakshyakumar168@users.noreply.github.com>
2025-12-27 21:58:20 +0530

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -1,14 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+
+
 class Question(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=200)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
-class Submission(models.Model):
+
+class Enrollment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    score = models.IntegerField(default=0
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
